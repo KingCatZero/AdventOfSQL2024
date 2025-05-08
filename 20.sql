fb47e8f61f83db05bc267dbe63be08da37210f85
KingCatZero <39886878+KingCatZero@users.noreply.github.com>
2025-05-08 21:37:51 +0100

    Create 20.sql

---
+++ 20.sql
@@ -0,0 +1,23 @@
+WITH
+	wr1 AS (
+		SELECT
+			*
+			,LIST_UNIQUE(
+				LIST_TRANSFORM(
+					STRING_SPLIT(
+						SPLIT_PART(url, '?', 2), '&'
+					)
+					,x -> SPLIT_PART(x, '=', 1)
+				)
+			) AS param_cnt
+		FROM web_requests
+		WHERE
+			url LIKE '%utm_source=advent-of-sql%'
+	)
+SELECT
+	url
+FROM wr1
+ORDER BY
+	param_cnt DESC
+	,url
+LIMIT 1
